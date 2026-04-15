a2f3113cea1e93afc42534a6321106ed86338ef3
Adam Callanan <a.callanan@sbcglobal.net>
2026-04-14 20:44:43 -0500

    remove image for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import gavelIcon from './assets/GAVEL-ICON.png'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div>
-          <img src={gavelIcon} className="gavel icon" alt="Gavel icon" />
-      </div>
       <h1>ORDER IN THE COURT</h1>
       <ul>
         <li>CREATE game</li>
